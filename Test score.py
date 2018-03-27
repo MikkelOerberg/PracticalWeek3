16982cabf4d343f9c0b21a364fac37b82799bfde
Mikkel Oerberg Hansen <mikkeloerberg.hansen@my.jcu.edu.au>
2018-03-27 18:50:39 +0800

    updated test scores to run with a function

--- Test score.py
+++ Test score.py
@@ -3,14 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# : Fix this!
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score < 50:
-    print("Bad")
+def main():
+
+    score = float(input("Enter score: "))
+    result = score_check(score)
+    print(result)
+
+
+def score_check(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score < 50:
+        return "Bad"
+
+
+main()
